0cecc4caeb0015c86cfbe73f197c2ff6e775c9e5
huyvawn <63355258+huyvawn@users.noreply.github.com>
2024-06-21 09:25:42 +0700

    update

---
+++ app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function UserProfile() {
+  return (
+    <div>
+      <h1>
+        Mark
+      </h1>
+      <img src="https://images.pexels.com/photos/1704488/pexels-photo-
+1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=".." class="user-profile-pic" />
+<h2>Hobbies</h2>
+<ul>
+  <li>Badminton</li>
+  <li>CHess</li>
+  <li>Gaming</li>
+</ul>
+    </div>
+  )
+}
+
+export default UserProfile;
